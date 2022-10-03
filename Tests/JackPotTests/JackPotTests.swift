80ca90f3706149a51e9ed9ee947cb08f8e4efc3a
marcprux <marcprux@users.noreply.github.com>
2022-10-03 16:52:46 +0000

    deploy: 10cf2a6bba1f07f11c7da252231cb502e46b675e

--- Tests/JackPotTests/JackPotTests.swift
+++ Tests/JackPotTests/JackPotTests.swift
@@ -21,7 +21,7 @@ final class JackPotTests: XCTestCase {
             let data = string.data(using: .utf8) ?? Data()
 
             do {
-                // in the rare case that a script consists exclusively of JSON,
+                // attempt to parse the whole data blob
                 return try decoder.decode(Metadata.self, from: data)
             } catch let error as NSError {
                 // the convention for decoding errors is that it is an NSError with a single underlying error, which itself has a `NSJSONSerializationErrorIndex` property with the failing index; this will be the fastest way to identify where a well-formed prelude JSON may have ended and the remainder of the script begun, so we first try to parse up to the initial JSON error
@@ -37,13 +37,21 @@ final class JackPotTests: XCTestCase {
                 }
                 #endif
 
+                // TODO: on Linux, this will be:
+                // dataCorrupted(Swift.DecodingError.Context(codingPath: [], debugDescription: "The given data was not valid JSON.", underlyingError: Optional(Foundation.JSONError.unexpectedCharacter(ascii: 49, characterIndex: 39))))
+                // we can try to oarse out the index, but `Foundation.JSONError` is not public
+
                 // we don't have access to the error index (e.g., we are running on Linux), so
                 // fall back to brute-force parsing it from the beginning up to every potentially-vaid closing
                 // character (which must be "}" since the metadata must be an object)
                 let closeBrace = "}".utf8.first
 
                 for index in data.indices.lazy.filter({ data[$0] == closeBrace }) {
-                    return try decoder.decode(Metadata.self, from: data[0...index])
+                    do {
+                        return try decoder.decode(Metadata.self, from: data[0...index])
+                    } catch {
+                        // ignore decoding errors during brute-force parse
+                    }
                 }
                 throw error
             }
