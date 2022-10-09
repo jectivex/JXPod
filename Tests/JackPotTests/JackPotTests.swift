b133dfa4b21325c0b2a3820f4df186bd7a950aa5
marcprux <mwp1@cornell.edu>
2022-10-08 22:26:35 -0400

    chore: checkpoint

--- Tests/JackPotTests/JackPotTests.swift
+++ Tests/JackPotTests/JackPotTests.swift
@@ -61,7 +61,7 @@ final class JackPotTests: XCTestCase {
         @discardableResult func succeed(_ string: String) -> JXContext {
             let ctx = JXContext(strict: true) // for script validation
             XCTAssertNoThrow(try decodePreamble(string), "preamble should have been decoded")
-            XCTAssertEqual(true, try ctx.check(string), "script should have been parsable")
+            XCTAssertEqual(true, try ctx.checkSyntax(string), "script should have been parsable")
             XCTAssertNoThrow(try ctx.eval(string), "script should have executed")
             return ctx
         }
